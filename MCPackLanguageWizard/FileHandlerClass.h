8d100dc2608ee6a3f9059c00d9803f38947fca94
Depozit <maxtop1924@yandex.ru>
2024-05-14 13:19:34 +0300

    rename ref dir

--- MCPackLanguageWizard/FileHandlerClass.h
+++ MCPackLanguageWizard/FileHandlerClass.h
@@ -7,7 +7,7 @@ namespace depozit_wizard {
 	class FileHandler {
 	protected:
 		//json
-		void initJson(unsigned short); //initialise the project settings base file with all input data
+		void initJson(std::string,fs::path,unsigned short); //initialise the project settings base file with all input data
 		void addJsonData(unsigned short); //adds flags to the project
 		std::vector<std::wstring> getJsonData(); //retrieves json data //I don't know the best way to get specific things yet
 
