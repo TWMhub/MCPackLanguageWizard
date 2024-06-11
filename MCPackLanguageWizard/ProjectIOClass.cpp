4e2aa1ad59f3dc10df0abb168f789e86c005fa01
Depozit <maxtop1924@yandex.ru>
2024-06-11 16:59:43 +0300

    -

--- MCPackLanguageWizard/ProjectIOClass.cpp
+++ MCPackLanguageWizard/ProjectIOClass.cpp
@@ -2,21 +2,82 @@
 
 namespace depozit_wizard {
 
-	void ProjectIO::initProject(std::string projectName, fs::path sourceDir, fs::path pathÑreation) {
-		this->projectName = projectName;
-		this->sourcePath = sourceDir;
-		this->projectPath = pathÑreation;
 
-		fs::create_directory(pathÑreation / projectName);
-		fs::create_directory(pathÑreation / projectName / "input");
-		fs::create_directory(pathÑreation / projectName / "output");
+	ProjectIO::ProjectIO(fs::path projetctsDir) {
 
-		fs::copy(sourceDir / "kubejs", pathÑreation / projectName / "input");
-		fs::copy(sourceDir / "config", pathÑreation / projectName / "input");
+		if (fs::exists(projetctsDir)) {
+
+			this->projectsDir = projetctsDir;
+
+			for (const auto entry : fs::directory_iterator(projetctsDir)) {
+				if (fs::is_directory(entry)) {
+					std::string path = entry.path().string();
+					if (path.substr(path.length() - 3) == "-pf") {
+						this->existingProjects.push_back(path.substr(path.length() - 3));
+					}
+				}
+			}
+
+		}
+			
 	}
 
-	void ProjectIO::deleteProject() {
-		fs::remove_all(projectPath / projectName);
+
+	void ProjectIO::createProject(std::string nameProject) {
+
+		for (int i = 0; i < this->existingProjects.size(); i++) {
+			if (nameProject == this->existingProjects[i])
+				return;
+		}
+
+		std::string projectId = nameProject + " -pf";
+
+		fs::create_directory(this->projectsDir / projectId);
+		fs::create_directories(this->projectsDir / projectId / "in");
+		fs::create_directories(this->projectsDir / projectId / "out");
+		std::ofstream(this->projectsDir / projectId / "translation file.pf").close();
+
+		this->existingProjects.push_back(nameProject);
+
+	}
+
+
+	void ProjectIO::initProject(std::string nameProject, fs::path pathToModpack) {
+
+		if (!fs::exists(this->projectsDir / (nameProject + " -pf")))
+			return;
+
+		if (!fs::exists(pathToModpack))
+			return;
+
+		if (!(fs::exists(pathToModpack / "config") && fs::exists(pathToModpack / "mods")))
+			return;
+
+		if (fs::exists(pathToModpack / "config/ftbquests/quests")) {
+
+			std::vector<std::wstring> translatableStrings;
+
+			FileHandler handler;
+			Wizard wizard;
+			//convert to yaml..
+			for (const auto entry : fs::directory_iterator(pathToModpack / "config/ftbquests/quests/chapters")) {
+				wizard.init(handler.readFile(entry), TargetInit::ftbQuests);
+				auto dataWizard = wizard.getAllFilesData();
+				for (const auto file : dataWizard) {
+					translatableStrings.push_back(strToWstr(file.pathToFile.string()));
+					for (auto lines : file.compiledTextLines) {
+						translatableStrings.push_back(std::to_wstring(lines.getPos()));
+						translatableStrings.push_back(lines.getOrigLine());
+						translatableStrings.push_back(lines.getLocaleLine());
+					}
+				}
+				wizard.clear();
+			}
+			
+
+		}
+
+
 	}
 
 }
--- MCPackLanguageWizard/ProjectIOClass.h
+++ MCPackLanguageWizard/ProjectIOClass.h
@@ -8,6 +8,8 @@
 * Instead of long attempts to understand the functionality of the library and adjustments to suit yourself,
 * I recommend to use the graphical application PolyFlow
 * 
+* 
+* 
 --------------------------------------------------------------------------*/
 
 #ifndef PROJECTIOCLASS_H
@@ -19,22 +21,55 @@ namespace depozit_wizard {
 
 	class ProjectIO {
 	public:
-		void initProject(std::string projectName, fs::path sourceDir, fs::path path—reation);
-		void deleteProject();
 
-		std::vector<std::pair<fs::path,std::vector<std::wstring>>> getOrigFiles();
-		std::vector<std::wstring> getProjectFile();
+		/*
+		* this method accepts a directory with projects of the PolyFlow type,
+		* and works only with it, if you need to read projects from another folder, 
+		* create another instance of the class
+		*/
+		ProjectIO(fs::path projetctsDir);
 
-		void setProjectFile(std::vector<std::wstring> data);
+		/*
+		* creates a PolyFlow project folder with the specified name, 
+		* and also generates standard files. 
+		* To import data from modpack, use the "initProject" function
+		*/
+		void createProject(std::string nameProject);
 
-		//file translation replacement
-		void configureOut();
+		/*
+		* initializes the specified project based on data from the specified modpack
+		* the path should point to the "minecraft" folder or, in the case of sourceforge, the name of the modpack.
+		* function checks for the presence of mods and config folders (in my experience these folders are always in modpacks.)
+		* to avoid errors I advise to use the function getExistringProjects more often.
+		*/
+		void initProject(std::string nameProject, fs::path pathToModpack);
+
+		/*
+		* returns a vector of all existing projects in the folder
+		* easy to use so that you don't get the name of the project wrong
+		*/
+		std::vector<std::string> getExistringProjects();
+
+		/*
+		* returns the read .pf project file, 
+		* if the project is not initialized, it will return an empty vector
+		*/
+		std::vector<std::wstring> getTranslationFile(std::string nameProject);
+
+		/*
+		* writes the updated translation file to the specified project
+		* if the project is not initialized, it will return an empty vector
+		*/
+		void setTranslationFile(std::string nameProject, std::vector<std::wstring>);
+
+		/*
+		* replaces the original lines with translated ones, and returns the original files but translated ones to the out folder.
+		*/
+		void configureTranslate(std::string nameProject);
 
 	private:
-		std::vector<std::wstring> projectFile;
-		std::string projectName;
-		fs::path projectPath;
-		fs::path sourcePath;
+		fs::path projectsDir;
+		std::vector<std::string> existingProjects;
 	};
 
 }
